d8c7338f7957e572e2d2d885c209bfdec6ece387
Agekyan <daagekyan@cpp.edu>
2024-11-18 11:52:42 +0000

    Updated and modified the following files: urls.py, views.py, requirements.txt (mainly setup for token-based --> JWT authentication for user login and registration).. RefreshToken model added to models.py for token refreshment. Reg and login views return both access and refresh tokens. (Removed logout view) and url path

--- user_authentication/urls.py
+++ user_authentication/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import UserRegistrationView, UserLoginView, UserLogoutView
+from .views import UserRegistrationView, UserLoginView
+from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
     path('register/', UserRegistrationView.as_view(), name='user-register'),
     path('login/', UserLoginView.as_view(), name='user-login'),
-    path('logout/', UserLogoutView.as_view(), name='user-logout'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token-refresh'),
 ]
