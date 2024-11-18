f63fcd6b0d05a4bea0cc48a7e39708714c6839a3
Agekyan <daagekyan@cpp.edu>
2024-11-18 11:23:55 +0000

    Forgotten simplejwst token in settings.py, added now.

--- server/settings.py
+++ server/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'user_authentication',
     'rest_framework',
     'rest_framework.authtoken',
+    'rest_framework_simplejwt',
     'vulnerabilities',
     'analysis',
     #'rest_framework_swagger',
@@ -113,6 +114,28 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+}
+
+from datetime import timedelta
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'ROTATE_REFRESH_TOKENS': False,
+    'BLACKLIST_AFTER_ROTATION': False,
+    'ALGORITHM': 'HS256',
+    'SIGNING_KEY': SECRET_KEY,
+    'VERIFYING_KEY': None,
+    'AUTH_HEADER_TYPES': ('Bearer',),
+    'USER_ID_FIELD': 'id',
+    'USER_ID_CLAIM': 'user_id',
+    'AUTH_TOKEN_CLASSES': ('rest_framework_simplejwt.tokens.AccessToken',),
+    'TOKEN_TYPE_CLAIM': 'token_type',
+}
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
