9e73494e611ca30fc62240510ee7891f51fc198a
Agekyan <daagekyan@cpp.edu>
2024-12-03 02:40:06 +0000

    Matched content: api-endpoints, backend projects accordingly to frontend vue and other commits, added new endpoint and url for projects. Testing two seperate (1 commented out) versions of the same function in views.py )

--- backend/projects/urls.py
+++ backend/projects/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import ProjectListCreateView, ProjectRetrieveUpdateDestroyView, UserProjectListView, ProjectUserAddView
+from .views import ProjectListCreateView, ProjectRetrieveUpdateDestroyView, UserProjectListView, ProjectUserAddView, InitiateScanView, LogoutView
 
 urlpatterns = [
     path('projects/', ProjectListCreateView.as_view(), name='project-list-create'),
     path('projects/<int:pk>/', ProjectRetrieveUpdateDestroyView.as_view(), name='project-detail'),
     path('users/<int:user_id>/projects/', UserProjectListView.as_view(), name='user-projects'),
     path('projects/<int:pk>/users/', ProjectUserAddView.as_view(), name='project-add-user'),
+    path('projects/<int:project_id>/scan/', InitiateScanView.as_view(), name='initiate-scan'),
+    path('logout/', LogoutView.as_view(), name='logout'),  # Add logout endpoint
 ]
