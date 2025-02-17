3c771734cb57ed46b44e47ab5533a9916dcc5d51
Agekyan <daagekyan@cpp.edu>
2025-02-17 08:23:28 +0000

    Return to port 5432

--- backend/server/settings.py
+++ backend/server/settings.py
@@ -91,7 +91,7 @@ DATABASES = {
         'USER': os.environ.get("POSTGRES_USER"),
         'PASSWORD': os.environ.get("POSTGRES_PASSWORD"),
         'HOST': os.environ.get("POSTGRES_HOST"),
-        'PORT': 5433,
+        'PORT': '5432',
     }
 }
 
