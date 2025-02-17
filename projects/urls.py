8807ec94ec36c275266c58d1abcc842fd240df3f
Agekyan <daagekyan@cpp.edu>
2025-02-17 23:38:21 +0000

    Refactored AnalysisResult and Vulnerability models

--- projects/urls.py
+++ projects/urls.py
@@ -1,5 +1,13 @@
 from django.urls import path
-from .views import ProjectListCreateView, ProjectRetrieveUpdateDestroyView, UserProjectListView, ProjectUserAddView, InitiateScanView, LogoutView
+from .views import (
+    ProjectListCreateView,
+    ProjectRetrieveUpdateDestroyView,
+    UserProjectListView,
+    ProjectUserAddView,
+    InitiateScanView,
+    ProjectVulnerabilitiesListView,
+    LogoutView
+)
 
 urlpatterns = [
     path('projects/', ProjectListCreateView.as_view(), name='project-list-create'),
@@ -7,5 +15,6 @@ urlpatterns = [
     path('users/<int:user_id>/projects/', UserProjectListView.as_view(), name='user-projects'),
     path('projects/<int:pk>/users/', ProjectUserAddView.as_view(), name='project-add-user'),
     path('projects/<int:project_id>/scan/', InitiateScanView.as_view(), name='initiate-scan'),
+    path('projects/<int:project_id>/vulnerabilities/', ProjectVulnerabilitiesListView.as_view(), name='project-vulnerabilities'),
     path('logout/', LogoutView.as_view(), name='logout'),
 ]
